6a522969c8da0091521cc532169880b50478a381
Jean-François Fabre <jotd666@gmail.com>
2026-04-20 17:56:05 +0200

    removed transitions!

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -5,9 +5,9 @@ from shared import *
 
 sprite_context_list = ["intro","level_1","level_2","level_3","level_3_base","level_4"]
 bg_tile_context_list = ["level_1_1","level_1_2","level_2","level_3_1","level_3_2","level_3_base",
-"level_4","outro","level_23_transition"]
-sprite_context_list = ["level_1","level_2","level_3"]
-bg_tile_context_list = ["level_1_2"]
+"level_4","outro"]
+sprite_context_list = []
+bg_tile_context_list = ["level_2"]
 
 sprite_names = get_sprite_names()
 
