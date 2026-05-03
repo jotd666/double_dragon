d126f5f73ead54247e81ceda425ccb0c3c50b5f7
Jean-François Fabre <jotd666@gmail.com>
2026-05-03 12:16:39 +0200

    ripped missing frames

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -7,7 +7,7 @@ from shared import *
 sprite_context_list = ["intro","level_1","level_2","level_3","level_3_base","level_4"]
 bg_tile_context_list = ["level_1_1","level_1_2","level_2","level_3_1","level_3_2","level_3_base",
 "level_4","outro"]
-sprite_context_list = ["level_1"]
+sprite_context_list = ["level_3_base"]
 bg_tile_context_list = []
 
 sprite_names = get_sprite_names()
